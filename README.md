c59bd1dfae9b5d96109820560e6fbcaf0b070529
osy <50960678+osy@users.noreply.github.com>
2020-03-25 23:34:47 -0700

    Added source files

---
+++ README.md
@@ -0,0 +1,30 @@
+altstore-github
+===============
+Builds a repository for [AltStore][1] from data in Github releases.
+
+## Configuration
+
+Look at `config.sample.json` for an example and `config.js` for the schema.
+
+## Launch as Server
+
+To start a repository as a server, create `config.json` and run:
+```
+npm start
+```
+
+You can also specify port, host, and a different config file name:
+```
+npm start -- --ip 0.0.0.0 --port 8080 --config custom.json
+```
+
+## Output repository file
+
+Alternatively, you can output the repository JSON directly:
+```
+node index.js --config custom.json
+```
+
+The repository JSON will be written to standard output.
+
+[1]: https://altstore.io
---
+++ config.js
@@ -0,0 +1,168 @@
+var convict = require('convict');
+
+convict.addFormat({
+  name: 'source-array',
+  validate: function(sources, schema) {
+    if (!Array.isArray(sources)) {
+      throw new Error('must be of type Array');
+    }
+
+    for (source of sources) {
+      convict(schema.children).load(source).validate();
+    }
+  }
+})
+
+// Define a schema
+var config = convict({
+  config: {
+    doc: "User specified config file to merge with",
+    format: String,
+    default: "config.json",
+    env: "CONFIG_FILE",
+    arg: "config"
+  },
+  ip: {
+    doc: "The IP address to bind.",
+    format: "ipaddress",
+    default: "127.0.0.1",
+    env: "IP_ADDRESS",
+    arg: "ip"
+  },
+  port: {
+    doc: "The port to bind.",
+    format: "port",
+    default: 8080,
+    env: "PORT",
+    arg: "port"
+  },
+  repo: {
+    name: {
+      doc: "Repository name",
+      format: String,
+      default: "AltStore Demo Source"
+    },
+    identifier: {
+      doc: "Repository identifier",
+      format: String,
+      default: "com.rileytestut.AltStore.Demo"
+    }
+  },
+  apps: {
+    doc: "Apps in the repository.",
+    format: "source-array",
+    default: [], 
+    children: {
+      name: {
+        doc: "App name",
+        format: String,
+        default: null
+      },
+      bundleIdentifier: {
+        doc: "App bundle identifier",
+        format: String,
+        default: null
+      },
+      githubOwner: {
+        doc: "App github owner",
+        format: String,
+        default: null
+      },
+      githubRepository: {
+        doc: "App github repository name where releases reside",
+        format: String,
+        default: null
+      },
+      developerName: {
+        doc: "App developer",
+        format: String,
+        default: null
+      },
+      subtitle: {
+        doc: "App subtitle",
+        format: String,
+        default: null
+      },
+      localizedDescription: {
+        doc: "App description",
+        format: String,
+        default: null
+      },
+      iconURL: {
+        doc: "App icon",
+        format: String,
+        default: null
+      },
+      tintColor: {
+        doc: "App color",
+        format: String,
+        default: null
+      },
+      screenshotURLs: {
+        doc: "App screenshots",
+        format: Array,
+        default: []
+      }
+    }
+  },
+  news: {
+    doc: "Repo news to show to users",
+    format: "source-array",
+    default: [],
+    children: {
+      title: {
+        doc: "News title",
+        format: String,
+        default: null
+      },
+      identifier: {
+        doc: "News slug",
+        format: String,
+        default: null
+      },
+      caption: {
+        doc: "News caption",
+        format: String,
+        default: null
+      },
+      tintColor: {
+        doc: "News tint color",
+        format: String,
+        default: null
+      },
+      imageURL: {
+        doc: "News image URL",
+        format: String,
+        default: ""
+      },
+      appID: {
+        doc: "News associated bundle id",
+        format: String,
+        default: null
+      },
+      date: {
+        doc: "News date in YYYY-MM-DD format",
+        format: String, //TODO: check date format
+        default: null
+      },
+      notify: {
+        doc: "Notify user of this news item",
+        format: Boolean,
+        default: false
+      }
+    }
+  }
+})
+
+// Load environment dependent configuration
+const user = config.get('config')
+if (user) {
+  config.loadFile(user)
+} else {
+  console.error("No config file loaded!")
+}
+
+// Perform validation
+config.validate({allowed: 'strict'})
+
+module.exports = config
